ccebec8a6173d5d88b7edc0a67529dac2a66f989
shashikantshah <shashikantshah777@gmail.com>
2023-02-11 11:33:01 +0530

    Update MediaQueriesWithJavaScript.js

--- MediaQueriesWithJavaScript.js
+++ MediaQueriesWithJavaScript.js
@@ -11,3 +11,31 @@ var x = window.matchMedia("(max-width: 700px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
 </script>
+
+
+//memoized
+<script>
+   var cache = {};
+        setInterval(function () {
+            var element = document.querySelector('.setStyle');
+            let WindowWidth = window.innerWidth - 20;
+            if (WindowWidth in cache) {
+                
+            } else {
+                cache[WindowWidth] = WindowWidth;
+                    if (WindowWidth < 768) {
+                        let setWidthValue = WindowWidth + "px";
+                        element.style.setProperty('width', setWidthValue);
+                    } else if (WindowWidth < 820) {
+                        element.style.setProperty('width', '340px');
+                    } else if (WindowWidth < 912) {
+                        element.style.setProperty('width', '380px');
+                    }
+                    else {
+                        element.style.setProperty('width', '420px');
+                    }
+            }
+          
+        });
+  <script>
+  
